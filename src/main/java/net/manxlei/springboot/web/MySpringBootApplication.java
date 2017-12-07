2758e440871a68412583cd5d8cdc207c6752ae64
Xue Bin Robin Lei <robin.x.b.lei@noexternalmail.hsbc.com>
2017-12-07 10:00:45 +0800

    fix issue

--- src/main/java/net/manxlei/springboot/web/MySpringBootApplication.java
+++ src/main/java/net/manxlei/springboot/web/MySpringBootApplication.java
@@ -2,11 +2,18 @@ package net.manxlei.springboot.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("net.manxlei.springboot.web")
-public class MySpringBootApplication {
+public class MySpringBootApplication extends SpringBootServletInitializer {
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MySpringBootApplication.class);
+    }
 
 	public static void main(String[] args) {
 		System.setProperty("spring.devtools.restart.enabled", "false");
